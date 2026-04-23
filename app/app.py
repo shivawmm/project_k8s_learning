d71700ac688914ea8b99afbb34e242a962e9fdfd
Shivam Singh <shivam@shertechinfo.com>
2026-04-23 19:13:34 +0530

    python file updated to dev branch

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Shivam's DevOps Project 🚀"
+    return "Hello from Shivam's DevOps Project 🚀 from DEV branch"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
