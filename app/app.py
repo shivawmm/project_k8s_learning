44126288c31d211a0a50cf3d91c66ab709e4a822
Shivam Singh <shivam@shertechinfo.com>
2026-04-23 19:32:26 +0530

    new image version 2.0 pushed

--- app/app.py
+++ app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Shivam's DevOps Project 🚀 - this is from PROD branch "
+    env = os.getenv("ENV", "UNKNOWN")
+    return f"Hello from Shivam's DevOps Project 🚀 - this is from {env} environment"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
