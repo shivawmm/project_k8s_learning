452a9442f67c31c61a73a368c1eeb87c563e7ceb
Shivam Singh <shivamsingh181002@outlook.com>
2026-04-27 15:37:13 +0530

    Email updated

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     env = os.getenv("ENV", "UNKNOWN")
-    return f"Hello from Shivam's DevOps Project 🚀 - this is from {env} environment, image auto update tag added, that means when any change happen CI will make a new image."
+    return f"Hello from Shivam's DevOps Project 🚀 - this is from {env} environment."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
