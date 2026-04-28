0217e089501009f1a0e71aa5c9e1412153792863
Shivam Singh <shivamsingh181002@outlook.com>
2026-04-28 16:31:35 +0530

    andi mandi sandi jo naa chale to uski maa ...

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     env = os.getenv("ENV", "UNKNOWN")
-    return f"Hello from Shivam's DevOps Project 🚀 - this is from {env} environment. abb to chal jaana bhai pls"
+    return f"Hello from Shivam's DevOps Project 🚀 - this is from {env} environment. abb to chal jaana bhai pls pls"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
